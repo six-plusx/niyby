c8122ca32898d58e26ec2933e189423ea68e32c0
QueXyan <46987257+QueXyan@users.noreply.github.com>
2019-01-29 15:50:10 +0800

    图书录入

--- Iyby/pages/qaddbooks/qaddbooks.js
+++ Iyby/pages/qaddbooks/qaddbooks.js
@@ -5,14 +5,154 @@ Page({
    * 页面的初始数据
    */
   data: {
+    booknames: '',//书名
+    authors: '',//作者
+    prices: '',//价格
+    messages: '',//出版社
+    nums: '',//录入数量
+    catalogues: '',//图书目录、
+    states: '',//图书状态
+       popErrorMsg: ''
+  },
 
+  // 书名
+  bookname(e) {
+    this.data.booknames = e.detail.value;
+  },
+
+  // 作者
+  author(e) {
+    this.data.authors = e.detail.value;
+  },
+
+  // 价格
+  price(e) {
+    this.data.prices = e.detail.value;
+  },
+
+  // 出版社
+  message(e) {
+    this.data.messages = e.detail.value;
+  },
+
+  // 录入数量
+  num(e) {
+    this.data.nums = e.detail.value;
+  },
+
+  //图书目录
+  catalogue(e) {
+    this.data.catalogues = e.detail.value;
+  },
+
+  // 图书状态
+  state(e) {
+    this.data.states = e.detail.value;
+  },
+
+//确认录入
+  addBook:function(){
+    if (this.data.booknames == '') {
+      setTimeout(() => {
+        this.setData(
+          { popErrorMsg: "请输入书名" }
+        );
+        this.ohShitfadeOut();
+      }, 100)
+      return;
+    }
+
+    if (this.data.authors == '') {
+      setTimeout(() => {
+        this.setData(
+          { popErrorMsg: "请输入作者" }
+        );
+        this.ohShitfadeOut();
+      }, 100)
+      return;
+    }
+
+    if (this.data.prices == '') {
+      setTimeout(() => {
+        this.setData(
+          { popErrorMsg: "请输入价格" }
+        );
+        this.ohShitfadeOut();
+      }, 100)
+      return;
+    }
+
+    if (this.data.messages == '') {
+      setTimeout(() => {
+        this.setData(
+          { popErrorMsg: "请输入出版社" }
+        );
+        this.ohShitfadeOut();
+      }, 100)
+      return;
+    }
+
+    if (this.data.nums == '') {
+      setTimeout(() => {
+        this.setData(
+          { popErrorMsg: "请输入数量" }
+        );
+        this.ohShitfadeOut();
+      }, 100)
+      return;
+    }
+
+    if (this.data.catalogues == '') {
+      setTimeout(() => {
+        this.setData(
+          { popErrorMsg: "请输入图书目录" }
+        );
+        this.ohShitfadeOut();
+      }, 100)
+      return;
+    }
+
+    if (this.data.states == '') {
+      setTimeout(() => {
+        this.setData(
+          { popErrorMsg: "请输入图书状态" }
+        );
+        this.ohShitfadeOut();
+      }, 100)
+      return;
+    }
+
+      wx.showToast({
+        title: '录入成功',
+        icon: 'success',
+       duration: 5000
+     })
+
+    //1秒后跳转
+    setTimeout(function () {
+      wx.reLaunch({
+        url: '../qstartentering/startentering',
+      })
+    }, 1000)
+    wx.switchTab({
+      url: '../qstartentering/startentering',
+    })
+  
+  },
+
+//错误提示方法
+  ohShitfadeOut() {
+    var fadeOutTimeout = setTimeout(() => {
+      this.setData({ popErrorMsg: '' });
+      clearTimeout(fadeOutTimeout);
+    }, 3000);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
