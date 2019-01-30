e79262999efefccbcfc0c71eb439cb94be28d073
FdyAngel <20000828@@fdy>
2019-01-30 11:44:50 +0800

    123

--- Six.Wx/Six.Wx.Api/Controllers/FDY/SearchController.cs
+++ Six.Wx/Six.Wx.Api/Controllers/FDY/SearchController.cs
@@ -5,10 +5,28 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using Six.Wx.IRepositoy.Fdy;
+using Six.Wx.Model;
+
 namespace Six.Wx.Api.Controllers.FDY
 {
     public class SearchController : ApiController
     {
+        /// <summary>
+        /// 属性注入
+        /// </summary>
+        public ISearch searchs { get; set; }
 
+        /// <summary>
+        /// 查询所有的图书信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ActionName("GetBooksSelects")]
+        public IEnumerable<BooksSelect> GetBooksSelects()
+        {
+            var books = searchs.GetBooksSelects();
+            return books;
+        }
     }
 }
--- Six.Wx/Six.Wx.Repositoy/Fdy/Search.cs
+++ Six.Wx/Six.Wx.Repositoy/Fdy/Search.cs
@@ -15,6 +15,24 @@ namespace Six.Wx.Repositoy.Fdy
 {
     public class Search : ISearch
     {
+        /// <summary>
+        /// 连接字符串语句
+        /// </summary>
+        public static string connStr = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
 
+        /// <summary>
+        /// 获取所有图书的信息
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<BooksSelect> GetBooksSelects()
+        {
+            string sql = "select * from BooksSelect";
+            //链接数据库
+            using (IDbConnection conn = new OracleConnection(connStr))
+            {
+                var booksSelectlist = conn.Query<BooksSelect>(sql);
+                return booksSelectlist;
+            }
+        }
     }
 }
