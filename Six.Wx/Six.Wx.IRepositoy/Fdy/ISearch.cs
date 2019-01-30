9fd2dc8ffcb3191183d39bebec3f554490bc8b23
FdyAngel <20000828@@fdy>
2019-01-30 11:06:58 +0800

    123

---
+++ Six.Wx/Six.Wx.IRepositoy/Fdy/ISearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Six.Wx.Model;
+
+namespace Six.Wx.IRepositoy.Fdy
+{
+    public interface ISearch
+    {
+        /// <summary>
+        /// 获取所有图书的信息
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<BooksSelect> GetBooksSelects();
+    }
+}
---
+++ Six.Wx/Six.Wx.Repositoy/Fdy/Search.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using Six.Wx.Model;
+using Dapper;
+using Six.Wx.IRepositoy.Fdy;
+using System.Data;
+using System.Data.OracleClient;
+
+namespace Six.Wx.Repositoy.Fdy
+{
+    public class Search : ISearch
+    {
+
+    }
+}
