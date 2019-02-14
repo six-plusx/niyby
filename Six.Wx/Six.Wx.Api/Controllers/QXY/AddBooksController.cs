eb56e308c2658c4786eeab001cb7e8b80b710e77
QueXyan <46987257+QueXyan@users.noreply.github.com>
2019-02-14 15:20:51 +0800

    控制器

---
+++ Six.Wx/Six.Wx.Api/Controllers/QXY/AddBooksController.cs
@@ -0,0 +1,42 @@
+﻿using Six.Wx.IRepositoy.Qxy;
+using Six.Wx.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Six.Wx.Api.Controllers.QXY
+{
+    public class AddBooksController : ApiController
+    {
+        /// <summary>
+        /// 属性注入
+        /// </summary>
+        public IAddBooksRepository addBooksRepository { get; set; }
+
+        /// <summary>
+        /// 添加图书
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public int AddBooks(BooksSelect booksSelect)
+        {
+            var books = addBooksRepository.AddBooks(booksSelect);
+            return books;
+        }
+
+        /// <summary>
+        /// 查询所有的图书信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<BooksSelect> GetBooksSelects()
+        {
+            var bookslist = addBooksRepository.GetBooksSelects();
+            return bookslist;
+        }
+
+    }
+}
