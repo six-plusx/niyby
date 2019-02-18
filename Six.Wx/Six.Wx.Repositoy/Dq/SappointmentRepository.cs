39bc9d2d9e150a3bc545f660872a999ca349769a
ShanQi123 <1311120907@qq.com>
2019-02-18 15:55:54 +0800

    Update SappointmentRepository.cs

--- Six.Wx/Six.Wx.Repositoy/Dq/SappointmentRepository.cs
+++ Six.Wx/Six.Wx.Repositoy/Dq/SappointmentRepository.cs
@@ -19,18 +19,13 @@ namespace Six.Wx.Repositoy.Dq
     public class SappointmentRepository:ISappointmentRepository
     {
         /// <summary>
-        /// 连接字符串
-        /// </summary>
-        public static string connStr = ConfigHelper.ConnString;
-
-        /// <summary>
         /// 查询所有图书
         /// </summary>
         /// <returns></returns>
         public IEnumerable<BooksSelect> GetAllBook()
         {
             string sql = "select *from booksselect b left join picture p on b.id=p.booksselectid";
-            using (IDbConnection conn=new OracleConnection(connStr))
+            using (IDbConnection conn=new OracleConnection(ConfigHelper.ConnString))
             {
                 var booksSelectlist = conn.Query<BooksSelect>(sql);
                 conn.Dispose();
@@ -47,7 +42,7 @@ namespace Six.Wx.Repositoy.Dq
         public IEnumerable<BooksSelect> GetBookById(int id)
         {
             string sql = $"select * from booksselect b left join picture p on b.id=p.booksselect.id where b.id='{id}'";
-            using (IDbConnection conn=new OracleConnection(connStr))
+            using (IDbConnection conn=new OracleConnection(ConfigHelper.ConnString))
             {
                 var booksSelectlist = conn.Query<BooksSelect>(sql);
                 conn.Close();
@@ -63,7 +58,7 @@ namespace Six.Wx.Repositoy.Dq
         public IEnumerable<BooksSelect> GetBookByName(string name,int typeid)
         {
             string sql = $"select * from BooksSelect b left join Picture p on b.id = p.booksselectid where BOOKSNAME like '%{name}%' and ClassifyId={typeid}";
-            using (IDbConnection conn=new OracleConnection(connStr))
+            using (IDbConnection conn=new OracleConnection(ConfigHelper.ConnString))
             {
                 var booksSelectlist = conn.Query<BooksSelect>(sql);
                 conn.Close();
